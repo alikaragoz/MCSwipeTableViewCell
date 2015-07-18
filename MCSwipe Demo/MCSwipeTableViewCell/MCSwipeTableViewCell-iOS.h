25db3658a3f927147448fd4ae98be6a82444dce3
Ali Karagoz <mail@alikaragoz.net>
2015-07-18 12:28:34 +0200

    Added support for dynamic frameworks
    And renamed the previous static library

---
+++ MCSwipe Demo/MCSwipeTableViewCell/MCSwipeTableViewCell-iOS.h
@@ -0,0 +1,19 @@
+//
+//  MCSwipeTableViewCell.h
+//  MCSwipeTableViewCell
+//
+//  Created by Ali Karagoz on 18/07/15.
+//  Copyright (c) 2015 Mad Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCSwipeTableViewCell.
+FOUNDATION_EXPORT double MCSwipeTableViewCellVersionNumber;
+
+//! Project version string for MCSwipeTableViewCell.
+FOUNDATION_EXPORT const unsigned char MCSwipeTableViewCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSwipeTableViewCell/PublicHeader.h>
+
+
